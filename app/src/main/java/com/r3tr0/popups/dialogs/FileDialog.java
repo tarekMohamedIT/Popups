1ddb64b983db3ec649fc7cf217bc6875835e10ce
r3tr0 <tarekmohamedit@gmail.com>
2018-04-19 09:03:47 +0200

    Added "selected file TextView"

--- app/src/main/java/com/r3tr0/popups/dialogs/FileDialog.java
+++ app/src/main/java/com/r3tr0/popups/dialogs/FileDialog.java
@@ -1,6 +1,5 @@
 package com.r3tr0.popups.dialogs;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Environment;
@@ -10,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
